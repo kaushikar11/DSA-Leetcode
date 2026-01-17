3284237b1e8e20836f2c1b18d0b74e18ac11adad
Kaushik <107872261+kaushikar11@users.noreply.github.com>
2026-01-17 13:49:14 -0700

    Time: 15 ms (73.69%) | Memory: 19.4 MB (7.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,34 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         maxLen = 0
+        
+#         for i in range(0,len(s)):
+#             h={}
+#             for j in range(i,len(s)):
+#                 if s[j] in h:
+#                     break
+#                     if maxLen<len(h):
+#                         maxLen = len(h)
+#                     h={}
+#                 h[s[j]] = 1
+#                 maxLen = max(maxLen,len(h))
+              
+#         return maxLen
+
+
+        
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        left = 0
         maxLen = 0
-        
-        for i in range(0,len(s)):
-            h={}
-            for j in range(i,len(s)):
-                if s[j] in h:
-                    break
-                    if maxLen<len(h):
-                        maxLen = len(h)
-                    h={}
-                h[s[j]] = 1
-                maxLen = max(maxLen,len(h))
-              
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+
+            charSet.add(s[right])
+            maxLen = max(maxLen, right - left + 1)
+
         return maxLen
-
-
-        
